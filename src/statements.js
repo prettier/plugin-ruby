c686a48be78410ecf56db2552693398128f5d5cf
Kevin Deisz <kevin.deisz@gmail.com>
2018-05-25 09:57:12 -0400

    Update to use the newer ripperjs version

--- src/statements.js
+++ src/statements.js
@@ -1,38 +1,13 @@
 const { concat, group, hardline, join } = require("prettier").doc.builders;
 
-const lineNoFrom = node => {
-  if (node === null) {
-    return null;
-  }
-
-  if (node.lineno) {
-    return node.lineno;
-  }
-
-  if (node.body) {
-    for (let idx = node.body.length - 1; idx >= 0; idx -= 1) {
-      const child = lineNoFrom(node.body[idx]);
-
-      if (child) {
-        return child;
-      }
-    }
-  }
-
-  return null;
-};
-
 const printStatementAdd = (path, options, print) => {
   const [leftStatement, rightStatement] = path.getValue().body;
-  const leftLineNo = lineNoFrom(leftStatement);
-
   let buffer = hardline;
-  if (leftLineNo) {
-    const rightLineNo = lineNoFrom(rightStatement);
 
-    if ((rightLineNo - leftLineNo) > 1) {
-      buffer = concat([hardline, hardline]);
-    }
+  // If there was originally multiple lines of whitespace between, compress down
+  // to just one line of whitespace.
+  if ((rightStatement.lineno - leftStatement.lineno) > 1) {
+    buffer = concat([hardline, hardline]);
   }
 
   if (leftStatement.type === "stmts_new") {
