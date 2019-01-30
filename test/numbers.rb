749efea1c5a677d573e256867f6c69bd4c72c096
Kevin Deisz <kevin.deisz@gmail.com>
2019-01-30 16:40:34 -0500

    Correct handling of numbers

--- test/numbers.rb
+++ test/numbers.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+# rubocop:disable Lint/Void
+
 123
 -123
 1_123
@@ -6,10 +10,12 @@
 123.45
 1.2e-3
 0xaabb
-0377
+0377 # this is actually octal
 -0b1010
 0b001_001
-?a
+?a # these are CHARs
 ?\C-a
 ?\M-a
 ?\M-\C-a
+
+# rubocop:enable Lint/Void
