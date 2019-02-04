99ffed7d641d60dfa0027b7d5f5b2e24531d1c1a
Kevin Deisz <kevin.deisz@gmail.com>
2019-02-04 10:09:43 -0500

    Simplify overall print function

--- src/print.js
+++ src/print.js
@@ -2,23 +2,20 @@ const { concat } = require("prettier").doc.builders;
 const nodes = require("./nodes");
 
 module.exports = (path, options, print) => {
-  const { type, body } = path.getValue();
-  const printer = nodes[type];
+  const { type, body, comment } = path.getValue();
 
-  if (!(type in nodes) && type[0] === "@") {
+  if (type in nodes) {
+    const printed = nodes[type](path, options, print);
+
+    if (comment) {
+      return concat([printed, path.call(print, "comment")]);
+    }
+    return printed;
+  }
+
+  if (type[0] === "@") {
     return body;
   }
 
-  if (!printer) {
-    throw new Error(`Unsupported node encountered: ${type}\n${JSON.stringify(body, null, 2)}`);
-  }
-
-  const printed = printer(path, options, print);
-
-  const { comment } = path.getValue();
-  if (comment) {
-    return concat([printed, path.call(print, "comment")]);
-  }
-
-  return printed;
+  throw new Error(`Unsupported node encountered: ${type}\n${JSON.stringify(body, null, 2)}`);
 };
