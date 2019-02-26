1f0cb1baff505f28ae45cfef40d6fd7eabe1397a
John Schoeman <johnschoeman1617@gmail.com>
2019-02-26 09:03:50 -0500

    Add minitest for assign.rb
    Why:
    We would like for the assign.rb file for the test snapshots to also be a
    minitest test so that we can run the ruby tests after the prettier
    formatting process an validate that the assignments are equivalent post
    formatting.
    
    this commit:
    Introduces minitest to '/test/cases/assign.rb'
    
    issue: [#120](https://github.com/prettier/plugin-ruby/issues/120)

--- test/cases/assign/assign.rb
+++ test/cases/assign/assign.rb
@@ -1,73 +1,123 @@
 # frozen_string_literal: true
 
 # rubocop:disable Lint/UselessAssignment, Style/ParallelAssignment
+# rubocop:disable Metrics/MethodLength
+# rubocop:disable Metrics/AbcSize
+# rubocop:disable Metrics/LineLength
 
-a = 1
+class AssignTest < Minitest::Test
+  def test_assignment
+    a = 1
+    assert_equal 1, a
 
-a =
-  begin
-    2
+    a =
+      begin
+        2
+      end
+    assert_equal 2, a
+
+    a, b, c = [1, 2, 3]
+    assert_equal 1, a
+    assert_equal 2, b
+    assert_equal 3, c
+
+    a = 1, 2, 3
+    assert_equal [1, 2, 3], a
+
+    a, b, c = 1, 2, 3
+    assert_equal 1, a
+    assert_equal 2, b
+    assert_equal 3, c
+
+    (a, b, c) = 1, 2, 3
+    assert_equal 1, a
+    assert_equal 2, b
+    assert_equal 3, c
+
+    ((a, b, c)) = 1, 2, 3
+    assert_equal 1, a
+    assert_equal 2, b
+    assert_equal 3, c
+
+    (((a, b, c))) = 1, 2, 3
+    assert_equal 1, a
+    assert_equal 2, b
+    assert_equal 3, c
+
+    a, *b = 1, 2, 3
+    assert_equal 1, a
+    assert_equal [2, 3], b
+
+    a, *b, c, d = 1, 2, 3
+    assert_equal 1, a
+    assert_equal [], b
+    assert_equal 2, c
+    assert_equal 3, d
+
+    a, * = 1, 2, 3
+    assert_equal 1, a
+
+    a = *a
+    assert_equal [1], a
+
+    (a, b), c = [1, 2], 3
+    assert_equal 1, a
+    assert_equal 2, b
+    assert_equal 3, c
+
+    * = [1, 2, 3]
+
+    *, a = [1, 2, 3]
+    assert_equal 3, a
+
+    super_super_super_super_super_long = 'bar'
+    super_super_super_long, super_super_super_long, super_super_super_long =
+      super_super_super_super_super_long, super_super_super_super_super_long, super_super_super_super_super_long
+    assert_equal super_super_super_long, 'bar'
+
+    a = 2
+    a ||= 1
+    assert_equal 2, a
+
+    a = 2
+    a ||= super_super_super_super_super_super_super_super_super_super_super_super_long
+    assert_equal 2, a
+
+    super_super_super_super_super_super_super_super_super_super_super_super_long = 'foo'
+    a = [
+      super_super_super_super_super_super_super_super_super_super_super_long,
+      super_super_super_super_super_super_super_super_super_super_super_long,
+      super_super_super_super_super_super_super_super_super_super_super_long
+    ]
+    assert_equal ['foo', 'foo', 'foo'], a
+
+    a = {
+      a: super_super_super_super_super_super_super_super_super_super_long,
+      b: super_super_super_super_super_super_super_super_super_super_long,
+      c: super_super_super_super_super_super_super_super_super_super_long
+    }
+    assert_equal "{ a: 'foo', b: 'foo', c: 'foo' }", a.to_s
+
+    # rubocop:disable Lint/UnneededCopDisableDirective
+    # rubocop:disable Layout/MultilineMethodCallIndentation
+    # I know, I know
+    super_super_super_super_long = 'baz'
+    a = [
+      super_super_super_super_long,
+      super_super_super_super_long
+    ].sort
+    assert_equal ['baz', 'baz'], a
+
+    a = {
+      a: super_super_super_super_long,
+      b: super_super_super_super_long
+    }.sort
+    assert_equal "{ a: 'baz', b: 'baz' }", a
+    # rubocop:enable Layout/MultilineMethodCallIndentation
+    # rubocop:enable Lint/UnneededCopDisableDirective
+
   end
-
-a, b, c = [1, 2, 3]
-
-a = 1, 2, 3
-
-a, b, c = 1, 2, 3
-
-(a, b, c) = 1, 2, 3
-
-((a, b, c)) = 1, 2, 3
-
-(((a, b, c))) = 1, 2, 3
-
-a, *b = 1, 2, 3
-
-a, *b, c, d = 1, 2, 3
-
-a, * = 1, 2, 3
-
-a = *a
-
-(a, b), c = [1, 2], 3
-
-* = [1, 2, 3]
-
-*, a = [1, 2, 3]
-
-super_super_super_long, super_super_super_long, super_super_super_long =
-  super_super_super_super_super_long, super_super_super_super_super_long, super_super_super_super_super_long
-
-a ||= 1
-
-a ||= super_super_super_super_super_super_super_super_super_super_super_super_long
-
-a = [
-  super_super_super_super_super_super_super_super_super_super_super_long,
-  super_super_super_super_super_super_super_super_super_super_super_long,
-  super_super_super_super_super_super_super_super_super_super_super_long
-]
-
-a = {
-  a: super_super_super_super_super_super_super_super_super_super_long,
-  b: super_super_super_super_super_super_super_super_super_super_long,
-  c: super_super_super_super_super_super_super_super_super_super_long
-}
-
-# rubocop:disable Lint/UnneededCopDisableDirective
-# rubocop:disable Layout/MultilineMethodCallIndentation
-# I know, I know
-a = [
-  super_super_super_super_long,
-  super_super_super_super_long
-].sort
-
-a = {
-  a: super_super_super_super_long,
-  b: super_super_super_super_long
-}.sort
-# rubocop:enable Layout/MultilineMethodCallIndentation
-# rubocop:enable Lint/UnneededCopDisableDirective
+end
 
 a.a = 1
 
@@ -75,3 +125,6 @@ super_super_super_long.super_super_super_super_super_super_super_long =
   super_super_super_super_super_super_super_super_super_super_super_long
 
 # rubocop:enable Lint/UselessAssignment, Style/ParallelAssignment
+# rubocop:enable Metrics/MethodLength
+# rubocop:enable Metrics/AbcSize
+# rubocop:enable Metrics/LineLength
