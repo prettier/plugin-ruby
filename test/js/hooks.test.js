d9e293b9eee06e45ec1d557f4d2b6fa2c5029778
Kevin Deisz <kevin.deisz@gmail.com>
2019-04-11 16:46:32 -0400

    Move over hooks tests

---
+++ test/js/hooks.test.js
@@ -0,0 +1,19 @@
+const { long } = require("./utils");
+
+describe.each(["BEGIN", "END"])("%s hook", hook => {
+  test("shortens to one line", () => (
+    expect(`${hook} {\n  p 'hook'\n}`).toChangeFormat(`${hook} { p 'hook' }`)
+  ));
+
+  test("maintains single lines", () => (
+    expect(`${hook} { p 'hook' }`).toMatchFormat()
+  ));
+
+  test("maintains multi line", () => (
+    expect(`${hook} {\n  ${long}\n}`).toMatchFormat()
+  ));
+
+  test("expands to multi line", () => (
+    expect(`${hook} { ${long} }`).toChangeFormat(`${hook} {\n  ${long}\n}`)
+  ));
+});
