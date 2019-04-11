prettier/plugin-ruby

commit bf11b5e90753ca918baddee07a3c8020d8ef40d2

So many fixes!

src/nodes.js
@@ -141,7 +141,17 @@ const nodes = {
       indent(concat([useNoSpace ? softline : line, path.call(print, "body", 2)]))
     ]));
   },
-  block_var: (path, opts, print) => concat(["|", removeLines(path.call(print, "body", 0)), "| "]),
+  block_var: (path, opts, print) => {
+    let parts = ["|", removeLines(path.call(print, "body", 0))];
+
+    // The second part of this node is a list of optional block-local variables
+    if (path.getValue().body[1]) {
+      parts.push("; ", join(", ", path.map(print, "body", 1)));
+    }
+
+    parts.push("| ");
+    return concat(parts);
+  },
   blockarg: (path, opts, print) => concat(["&", path.call(print, "body", 0)]),
   bodystmt: (path, opts, print) => {
     const [_statements, rescue, elseClause, ensure] = path.getValue().body;
@@ -232,11 +242,11 @@ const nodes = {
     "...",
     path.getValue().body[1] ? path.call(print, "body", 1) : ""
   ]),
-  dyna_symbol: (path, opts, print) => concat([
-    ":\"",
-    concat(path.call(print, "body", 0)),
-    "\""
-  ]),
+  dyna_symbol: (path, opts, print) => {
+    const { quote } = path.getValue().body[0];
+
+    return concat([`:${quote}`, concat(path.call(print, "body", 0)), quote]);
+  },
   else: (path, opts, print) => {
     const stmts = path.getValue().body[0];
 
@@ -329,13 +339,8 @@ const nodes = {
       indent(concat([line, right]))
     ]));
   },
-  method_add_arg: (path, opts, print) => {
-    if (path.getValue().body[1].type === "args_new") {
-      return path.call(print, "body", 0);
-    }
-    return group(concat(path.map(print, "body")));
-  },
-  method_add_block: (path, opts, print) => concat(path.map(print, "body")),
+  method_add_arg: concatBody,
+  method_add_block: concatBody,
   methref: (path, opts, print) => join(".:", path.map(print, "body")),
   mlhs: makeList,
   mlhs_add_post: (path, opts, print) => (

src/nodes/calls.js
@@ -2,6 +2,8 @@ const { concat, group, indent, softline } = require("prettier").doc.builders;
 const { makeCall } = require("../utils");
 
 const noIndent = [
+  "array",
+  "hash",
   "method_add_block",
   "xstring_literal"
 ];

test/cases/__snapshots__/blocks.test.js.snap
@@ -41,6 +41,8 @@ loop { super_super_super_super_super_super_super_super_super_super_super_long }
 
 loop { |i| 1 }
 
+loop { |i; j| 1 }
+
 loop { |i| i }
 
 loop { |*| i }
@@ -64,6 +66,16 @@ some_method.each do |foo|
   bar
   baz
 end.to_i
+
+[
+  super_super_super_super_super_super_super_super_super_super_long,
+  super_super_super_super_super_super_super_super_super_super_long
+].to_s
+
+{
+  a: 'super_super_super_super_super_super_super_super_super_super_long',
+  b: 'super_super_super_super_super_super_super_super_super_super_long'
+}.to_s
 
 loop(&:to_s)
 

test/cases/__snapshots__/strings.test.js.snap
@@ -63,6 +63,7 @@ exports[`strings.rb matches expected output 1`] = `
 { \\"a #{a}\\" => 1 }
 
 :\\"abc#{abc}abc\\"
+:'abc#{abc}abc'
 
 \`abc\`
 \`super_super_super_super_super_super_super_super_super_super_super_super_s_long\`
@@ -233,6 +234,7 @@ exports[`strings.rb matches expected output 2`] = `
 { \\"a #{a}\\" => 1 }
 
 :\\"abc#{abc}abc\\"
+:'abc#{abc}abc'
 
 \`abc\`
 \`super_super_super_super_super_super_super_super_super_super_super_super_s_long\`

test/cases/blocks.rb
@@ -44,6 +44,8 @@ end
 
 loop { |i| 1 }
 
+loop { |i; j| 1 }
+
 loop do |i|
   i
 end
@@ -69,6 +71,16 @@ some_method.each do |foo|
   bar
   baz
 end.to_i
+
+[
+  super_super_super_super_super_super_super_super_super_super_long,
+  super_super_super_super_super_super_super_super_super_super_long
+].to_s
+
+{
+  a: 'super_super_super_super_super_super_super_super_super_super_long',
+  b: 'super_super_super_super_super_super_super_super_super_super_long'
+}.to_s
 
 loop { |i| i.to_s }
 

test/cases/strings.rb
@@ -60,6 +60,7 @@
 { "a #{a}" => 1 }
 
 :"abc#{abc}abc"
+:'abc#{abc}abc'
 
 %x[abc]
 %x[super_super_super_super_super_super_super_super_super_super_super_super_s_long]

commit 6a2440cedaaa415cd6d50e37e3edd604a2f2240f

enable string symbol keys in hashes
For some reason, the parser classes `:'x'` as an `xstring`, but the key
part of { 'x': 1 } as a string. That also, for some reason, meant the
the `on_tstring_end` in the parser was not adding the relevant quote to
the sexp.

I added a very similar function to the parser, but for labels. All the
existing tests pass. I don't think there is a clean way of determining
when/where we can strip quotes from the keys, so I've had to disable the
linter to stop errors when running in hash rocket mode.

Fixes a regression from bf11b5e90753ca918baddee07a3c8020d8ef40d2
Closes #242

src/nodes.js
@@ -84,6 +84,16 @@ const nodes = {
           parts.push(concat([printedLabel, " =>"]));
         }
         break;
+      case "dyna_symbol":
+        if (preferHashLabels) {
+          parts.push(concat(
+            printedLabel.parts.slice(1) // get rid of the : at the start
+              .concat(":"),
+          ));
+        } else {
+          parts.push(concat([printedLabel, " =>"]));
+        }
+        break;
       default:
         parts.push(concat([printedLabel, " =>"]));
         break;
@@ -245,7 +255,7 @@ const nodes = {
   dyna_symbol: (path, opts, print) => {
     const { quote } = path.getValue().body[0];
 
-    return concat([`:${quote}`, concat(path.call(print, "body", 0)), quote]);
+    return concat([":", quote, concat(path.call(print, "body", 0)), quote]);
   },
   else: (path, opts, print) => {
     const stmts = path.getValue().body[0];

src/ripper.rb
@@ -377,6 +377,11 @@ module Layer
     def on_tstring_end(quote)
       last_sexp.merge!(quote: quote)
     end
+
+    def on_label_end(quote)
+      # `quote` is ": or ':
+      last_sexp.merge!(quote: quote[0])
+    end
   end
 end
 

test/cases/__snapshots__/string_symbol_key.test.js.snap
@@ -1,0 +1,43 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`string_symbol_key.rb matches expected output 1`] = `
+"# frozen_string_literal: true
+
+# rubocop:disable Lint/UnneededCopDisableDirective
+# rubocop:disable Style/SymbolLiteral
+
+def foo
+  {
+    'test': 1,
+    'special-key': 2,
+    \\"double-quote\\": 3,
+    \\"#{1 + 1}\\": 4,
+    \\"non-hash-label\\": 5
+  }
+end
+
+# rubocop:enable Style/SymbolLiteral
+# rubocop:enable Lint/UnneededCopDisableDirective
+"
+`;
+
+exports[`string_symbol_key.rb matches expected output 2`] = `
+"# frozen_string_literal: true
+
+# rubocop:disable Lint/UnneededCopDisableDirective
+# rubocop:disable Style/SymbolLiteral
+
+def foo
+  {
+    :'test' => 1,
+    :'special-key' => 2,
+    :\\"double-quote\\" => 3,
+    :\\"#{1 + 1}\\" => 4,
+    :\\"non-hash-label\\" => 5
+  }
+end
+
+# rubocop:enable Style/SymbolLiteral
+# rubocop:enable Lint/UnneededCopDisableDirective
+"
+`;

test/cases/string_symbol_key.rb
@@ -1,0 +1,17 @@
+# frozen_string_literal: true
+
+# rubocop:disable Lint/UnneededCopDisableDirective
+# rubocop:disable Style/SymbolLiteral
+
+def foo
+  {
+    'test': 1,
+    'special-key': 2,
+    "double-quote": 3,
+    "#{1 + 1}": 4,
+    :"non-hash-label" => 5
+  }
+end
+
+# rubocop:enable Style/SymbolLiteral
+# rubocop:enable Lint/UnneededCopDisableDirective

test/cases/string_symbol_key.test.js
@@ -1,0 +1,2 @@
+runCase("string_symbol_key.rb");
+runCase("string_symbol_key.rb", { preferHashLabels: false }, "hash-rockets.yml");