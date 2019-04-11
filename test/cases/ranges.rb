f27c9026f35aae5f59900c67bee43b508c34fc90
Kevin Deisz <kevin.deisz@gmail.com>
2019-04-11 16:18:23 -0400

    Move range tests over

--- test/cases/ranges.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# rubocop:disable Lint/UselessAssignment
-
-dot2 = 1..2 # dot2
-dot3 = 3...4 # dot3
-
-# rubocop:enable Lint/UselessAssignment
---
+++ test/js/ranges.test.js
@@ -0,0 +1,9 @@
+describe("ranges", () => {
+  test("two dot", () => (
+    expect("1..2").toMatchFormat()
+  ));
+
+  test("three dot", () => (
+    expect("3...4").toMatchFormat()
+  ));
+});
