364dd7188e9a0c0d8a4381c844f3fddf8141cbb2
Kevin Deisz <kevin.deisz@gmail.com>
2019-04-30 10:30:38 -0400

    Add @xipgroc as a contributor

--- src/nodes.js
+++ src/nodes.js
@@ -329,12 +329,14 @@ const nodes = {
     const proc = toProc(block);
 
     if (proc && method.type === "call") {
-      return group(concat([
-        path.call(print, "body", 0),
-        "(",
-        indent(concat([softline, proc])),
-        concat([softline, ")"])
-      ]));
+      return group(
+        concat([
+          path.call(print, "body", 0),
+          "(",
+          indent(concat([softline, proc])),
+          concat([softline, ")"])
+        ])
+      );
     }
     if (proc) {
       return path.call(print, "body", 0);
