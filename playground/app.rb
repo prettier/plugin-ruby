7df14147f6016d1dc28c80c284d8f32d5a5f08d8
Kevin Deisz <kevin.deisz@gmail.com>
2019-11-05 14:46:14 -0500

    Add the basic playground app

---
+++ playground/app.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# This is a small webapp whose only function is to generate the AST used by the
+# ruby plugin. It will be used by the prettier playground in order to format
+# stuff in ruby (since we can't do that client-side).
+
+require 'bundler/setup'
+require 'sinatra'
+
+require_relative '../src/ripper'
+
+class App < Sinatra::Base
+  get '/' do
+    halt 200
+  end
+
+  post '/' do
+    builder = RipperJS.new(request.body.read)
+    response = builder.parse
+
+    if !response || builder.error?
+      halt 422
+    else
+      JSON.fast_generate(response)
+    end
+  end
+end
+
+App.run!
