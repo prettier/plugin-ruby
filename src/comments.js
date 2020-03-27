6367a1cc911d39db1ff680e0a4c3d214a27a1ba7
Kevin Deisz <kevin.deisz@gmail.com>
2020-03-27 13:36:01 -0400

    Ensure comments work on empty statements nodes

--- src/comments.js
+++ src/comments.js
@@ -1,4 +1,16 @@
+const { addLeadingComment } = require("./prettier");
+
+const isEmptyStmts = (node) =>
+  node.type === "stmts" &&
+  node.body.length === 1 &&
+  node.body[0].type === "void_stmt";
+
 const handleOwnLineComment = (comment, text, opts, ast, isLastComment) => {
+  if (isEmptyStmts(comment.precedingNode)) {
+    addLeadingComment(comment.precedingNode, comment);
+    return true;
+  }
+
   return false;
 };
 
--- src/nodes/scopes.js
+++ src/nodes/scopes.js
@@ -9,16 +9,21 @@ const {
 
 module.exports = {
   class: (path, opts, print) => {
-    const [_constant, superclass, statements] = path.getValue().body;
+    const [_constant, superclass, bodystmt] = path.getValue().body;
+    const stmts = bodystmt.body[0];
 
     const parts = ["class ", path.call(print, "body", 0)];
     if (superclass) {
       parts.push(" < ", path.call(print, "body", 1));
     }
 
-    // If the body is empty, we can replace with a ;
-    const stmts = statements.body[0].body;
-    if (stmts.length === 1 && stmts[0].type === "void_stmt") {
+    if (
+      stmts.body.length === 1 &&
+      stmts.body[0].type === "void_stmt" &&
+      !stmts.comments
+    ) {
+      // The body is empty and does not contain any comments, so instead we can
+      // just replace the body with a semi-colon.
       return group(concat([concat(parts), ifBreak(line, "; "), "end"]));
     }
 
