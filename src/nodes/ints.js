b4b38dcba7b43207c4210185c1e038fc272395ac
Kevin Deisz <kevin.deisz@gmail.com>
2020-04-06 20:01:46 -0400

    Dedent and call it printInt

--- src/nodes/ints.js
+++ src/nodes/ints.js
@@ -1,24 +1,26 @@
-module.exports = {
-  "@int": (path, _opts, _print) => {
-    const { body } = path.getValue();
+const printInt = (path, _opts, print) => {
+  const { body } = path.getValue();
 
-    // If the number is octal and does not contain the optional "o" character
-    // after the leading 0, add it in.
-    if (/^0[0-9]/.test(body)) {
-      return `0o${body.slice(1)}`;
-    }
-
-    // If the number is a base 10 number, is sufficiently large, and is not
-    // already formatted with underscores, then add them in in between the
-    // numbers every three characters starting from the right.
-    if (!body.startsWith("0") && body.length >= 5 && !body.includes("_")) {
-      return `  ${body}`
-        .slice((body.length + 2) % 3)
-        .match(/.{3}/g)
-        .join("_")
-        .trim();
-    }
-
-    return body;
+  // If the number is octal and does not contain the optional "o" character
+  // after the leading 0, add it in.
+  if (/^0[0-9]/.test(body)) {
+    return `0o${body.slice(1)}`;
   }
+
+  // If the number is a base 10 number, is sufficiently large, and is not
+  // already formatted with underscores, then add them in in between the
+  // numbers every three characters starting from the right.
+  if (!body.startsWith("0") && body.length >= 5 && !body.includes("_")) {
+    return `  ${body}`
+      .slice((body.length + 2) % 3)
+      .match(/.{3}/g)
+      .join("_")
+      .trim();
+  }
+
+  return body;
+};
+
+module.exports = {
+  "@int": printInt
 };
