f9ec5b7a13a7bacecf0342ee25c2ddd56c883216
Kevin Deisz <kevin.deisz@gmail.com>
2020-04-06 20:26:03 -0400

    Comments on hooks

--- src/utils.js
+++ src/utils.js
@@ -45,6 +45,7 @@ const hasAncestor = (path, types) => {
 };
 
 const isEmptyStmts = (node) =>
+  node &&
   node.type === "stmts" &&
   node.body.length === 1 &&
   node.body[0].type === "void_stmt";
