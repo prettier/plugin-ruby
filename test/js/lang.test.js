abb66453f09833222a052ee7974c02921730ad60
Kevin Deisz <kevin.deisz@gmail.com>
2020-08-21 09:17:35 -0400

    Ensure we're parsing ruby files using UTF-8 regardless of the system encoding.

---
+++ test/js/lang.test.js
@@ -0,0 +1,27 @@
+const { spawnSync } = require("child_process");
+const path = require("path");
+
+expect.extend({
+  toHaveExitedCleanly(child) {
+    return {
+      pass: child.status === 0,
+      message: () => child.stderr.toString()
+    };
+  }
+});
+
+test("different lang settings don't break", () => {
+  const script = path.join(__dirname, "../../node_modules/.bin/prettier");
+  const child = spawnSync(
+    process.execPath,
+    [script, "--plugin", ".", "--parser", "ruby"],
+    {
+      env: {
+        LANG: "US-ASCII"
+      },
+      input: "'# あ'"
+    }
+  );
+
+  expect(child).toHaveExitedCleanly();
+});
