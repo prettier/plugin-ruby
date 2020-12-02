bc885ca6b582a35b686f46d9eb4e7443d5f8f687
Kevin Deisz <kevin.deisz@gmail.com>
2020-12-02 09:36:44 -0500

    Go through the whole cycle when testing code with comments in it

--- test/js/regexp.test.js
+++ test/js/regexp.test.js
@@ -26,7 +26,7 @@ describe("regexp", () => {
       /\\A
         [[:digit:]]+ # 1 or more digits before the decimal point
         (\\.         # Decimal point
-         [:digit:]]+ # 1 or more digits after the decimal point
+        [[:digit:]]+ # 1 or more digits after the decimal point
         )? # The decimal point and following digits are optional
       \\Z/x
     `);
