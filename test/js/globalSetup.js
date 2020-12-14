981157927aae5b0efa7ed2012748921d4688b512
Kevin Deisz <kevin.deisz@gmail.com>
2020-12-14 12:27:41 -0500

    Async parser for test code

---
+++ test/js/globalSetup.js
@@ -0,0 +1,14 @@
+const { spawn, spawnSync } = require("child_process");
+
+// Set a RUBY_VERSION environment variable because certain tests will only run
+// for certain versions of Ruby.
+const args = ["--disable-gems", "-e", "puts RUBY_VERSION"];
+process.env.RUBY_VERSION = spawnSync("ruby", args).stdout.toString().trim();
+
+function globalSetup() {
+  // Spawn the async parser process so that tests can send their content over to
+  // it to get back the AST.
+  global.__ASYNC_PARSER__ = spawn("ruby", ["./test/js/parser.rb"]);
+}
+
+module.exports = globalSetup;
