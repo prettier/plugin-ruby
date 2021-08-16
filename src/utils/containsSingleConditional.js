019ec2b68c70a32528fd86e69542ddec862c3a16
Nat Budin <natbudin@gmail.com>
2021-08-16 13:28:16 -0700

    Use block form when there's a ternary inside a conditional

--- src/utils/containsSingleConditional.js
+++ src/utils/containsSingleConditional.js
@@ -1,8 +1,10 @@
-// If the statements are just a single if/unless, in block or modifier form
+// If the statements are just a single if/unless, in block or modifier form, or a ternary
 function containsSingleConditional(statements) {
   return (
     statements.body.length === 1 &&
-    ["if", "if_mod", "unless", "unless_mod"].includes(statements.body[0].type)
+    ["if", "if_mod", "ifop", "unless", "unless_mod"].includes(
+      statements.body[0].type
+    )
   );
 }
 
