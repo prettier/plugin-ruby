66d76b938bd4a67f625981fbdf3620945fd3237d
Kevin Newton <kddnewton@gmail.com>
2021-09-21 10:13:47 -0400

    Revert netcat back to JavaScript

--- src/parser/netcat.js
+++ src/parser/netcat.js
@@ -2,10 +2,10 @@
 // On average, this is 2-3x slower than netcat, but still much faster than
 // spawning a new Ruby process.
 
-import { Socket, createConnection } from "net";
+const { createConnection } = require("net");
 
 const sock = process.argv[process.argv.length - 1];
-const client: Socket = createConnection(sock, () => process.stdin.pipe(client));
+const client = createConnection(sock, () => process.stdin.pipe(client));
 
 client.on("data", (data) => process.stdout.write(data));
 client.on("error", (error) => {
